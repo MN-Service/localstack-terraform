117dfe94c5ce6157e12dcf5a3a2266a45339f95c
Mohammad Noroozi <mm.74noroozi@gmail.com>
2023-07-05 23:18:17 +0330

    fix: change endpoint_url

--- main.tf
+++ main.tf
@@ -14,29 +14,29 @@ provider "aws" {
 
 
   endpoints {
-    # apigateway     = var.endpoint_url
-    # apigatewayv2   = var.endpoint_url
-    # cloudformation = var.endpoint_url
-    # cloudwatch     = var.endpoint_url
+    apigateway     = var.endpoint_url
+    apigatewayv2   = var.endpoint_url
+    cloudformation = var.endpoint_url
+    cloudwatch     = var.endpoint_url
     dynamodb       = var.endpoint_url
-    # ec2            = var.endpoint_url
-    # es             = var.endpoint_url
-    # elasticache    = var.endpoint_url
-    # firehose       = var.endpoint_url
-    # iam            = var.endpoint_url
-    # kinesis        = var.endpoint_url
-    # lambda         = var.endpoint_url
-    # rds            = var.endpoint_url
-    # redshift       = var.endpoint_url
-    # route53        = var.endpoint_url
-    # s3             = var.endpoint_url
-    # secretsmanager = var.endpoint_url
-    # ses            = var.endpoint_url
-    # sns            = var.endpoint_url
-    # sqs            = var.endpoint_url
-    # ssm            = var.endpoint_url
-    # stepfunctions  = var.endpoint_url
-    # sts            = var.endpoint_url
+    ec2            = var.endpoint_url
+    es             = var.endpoint_url
+    elasticache    = var.endpoint_url
+    firehose       = var.endpoint_url
+    iam            = var.endpoint_url
+    kinesis        = var.endpoint_url
+    lambda         = var.endpoint_url
+    rds            = var.endpoint_url
+    redshift       = var.endpoint_url
+    route53        = var.endpoint_url
+    s3             = var.endpoint_url
+    secretsmanager = var.endpoint_url
+    ses            = var.endpoint_url
+    sns            = var.endpoint_url
+    sqs            = var.endpoint_url
+    ssm            = var.endpoint_url
+    stepfunctions  = var.endpoint_url
+    sts            = var.endpoint_url
   }
 }
 
@@ -73,4 +73,13 @@ resource "aws_sns_topic_subscription" "private_http_subscription" {
   protocol  = "sqs"
   endpoint  = aws_sqs_queue.queue.arn 
   endpoint_auto_confirms = true
+}
+
+resource "aws_s3_bucket" "mnservice-bucket" {
+  bucket = "mnservice-bucket"
+
+  tags = {
+    Name        = "mnservice-bucket"
+    Environment = "Dev"
+  }
 }
