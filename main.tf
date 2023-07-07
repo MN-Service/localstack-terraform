8d2661a619ca2a75f03c0108ea07496149e22600
Mohammad Noroozi <mm.74noroozi@gmail.com>
2023-07-07 14:04:41 +0330

    fix: project structure and lock file

--- main.tf
+++ main.tf
@@ -1,45 +1,3 @@
-variable "endpoint_url" {
-  type        = string
-  description = "The URL of the endpoint"
-  default     = "http://localhost:4566"
-}
-
-provider "aws" {
-  region                      = "eu-central-1"
-  access_key                  = "mnservice"
-  secret_key                  = "mnservice"
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-
-  endpoints {
-    apigateway     = var.endpoint_url
-    apigatewayv2   = var.endpoint_url
-    cloudformation = var.endpoint_url
-    cloudwatch     = var.endpoint_url
-    dynamodb       = var.endpoint_url
-    ec2            = var.endpoint_url
-    es             = var.endpoint_url
-    elasticache    = var.endpoint_url
-    firehose       = var.endpoint_url
-    iam            = var.endpoint_url
-    kinesis        = var.endpoint_url
-    lambda         = var.endpoint_url
-    rds            = var.endpoint_url
-    redshift       = var.endpoint_url
-    route53        = var.endpoint_url
-    s3             = var.endpoint_url
-    secretsmanager = var.endpoint_url
-    ses            = var.endpoint_url
-    sns            = var.endpoint_url
-    sqs            = var.endpoint_url
-    ssm            = var.endpoint_url
-    stepfunctions  = var.endpoint_url
-    sts            = var.endpoint_url
-  }
-}
-
 ######### DynamoDB #########
 
 resource "aws_dynamodb_table" "mn-service-dev-consumer-events" {
@@ -79,7 +37,7 @@ resource "aws_s3_bucket" "mnservice-bucket" {
   bucket = "s3-1"
 
   tags = {
-    Name        = "mnservice-bucket"
+    Name        = "s3-1"
     Environment = "Dev"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "endpoint_url" {
+  type        = string
+  description = "The URL of the endpoint"
+  default     = "http://localhost:4566"
+}
