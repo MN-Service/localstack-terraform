b5fb321e17978a49b6d0c27d110d04820f3a7a6e
MO.Norouzi <mo.norouzi@emofid.com>
2023-07-10 17:35:30 +0330

    fix: s3_endpoint_url missing

--- variables.tf
+++ variables.tf
@@ -3,3 +3,10 @@ variable "endpoint_url" {
   description = "The URL of the endpoint"
   default     = "http://localhost:4566"
 }
+
+variable "s3_endpoint_url" {
+  type        = string
+  description = "The URL of the endpoint"
+  default     = "http://localhost:4566"
+}
+
