d473095afa8c6f2344e47fa477ec7b49520e86af
MO.Norouzi <mo.norouzi@emofid.com>
2023-07-23 12:29:36 +0330

    feat: s3 ddns config

--- variables.tf
+++ variables.tf
@@ -7,6 +7,6 @@ variable "endpoint_url" {
 variable "s3_endpoint_url" {
   type        = string
   description = "The URL of the endpoint"
-  default     = "http://localhost:4566"
+  default     = "http://s3.localhost.localstack.cloud:4566"
 }
 
